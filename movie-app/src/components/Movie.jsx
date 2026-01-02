76ebfd3983ddb960d0271c484362899132286a57
moonbyeongmin <qudalsrnt3x@gmail.com>
2026-01-02 12:16:00 +0900

    [7-7] useParams 및 css

--- movie-app/src/components/Movie.jsx
+++ movie-app/src/components/Movie.jsx
@@ -1,19 +1,22 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
 function Movie({ id, coverImg, title, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.img} />
+      <h2 className={styles.movie__title}>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
-      <ul>
+      <ul className={styles.movie__genres}>
         {genres.map((g) => (
           <li key={g}>{g}</li>
         ))}
       </ul>
+      <p className={styles.movie__summary}>
+        {summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}
+      </p>
     </div>
   );
 }
--- movie-app/src/routes/Detail.jsx
+++ movie-app/src/routes/Detail.jsx
@@ -1,5 +1,57 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css"; // CSS import 추가
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const json = await (
+        await fetch(`https://yts.lt/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+
+      setMovie(json.data.movie);
+      setLoading(false);
+    };
+    getMovie();
+  }, [id]);
+
+  return (
+    <div className={styles.container}>
+      {loading ? (
+        <h1 className={styles.loader}>Loading...</h1>
+      ) : (
+        <>
+          <img
+            src={movie.large_cover_image}
+            className={styles.poster}
+            alt={movie.title}
+          />
+          <h1 className={styles.title}>
+            {movie.title} ({movie.year})
+          </h1>
+          <div className={styles.info}>
+            <span>Rating: {movie.rating} / 10 </span> |
+            <span> {movie.runtime} min</span>
+          </div>
+          <ul className={styles.genres}>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+          <p className={styles.description}>{movie.description_full}</p>
+          <button
+            className={styles.back_btn}
+            onClick={() => window.history.back()}>
+            Go Back
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
